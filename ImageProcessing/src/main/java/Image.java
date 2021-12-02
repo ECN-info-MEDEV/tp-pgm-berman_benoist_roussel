a00957217425d8c795b79393c552323bfb0c30e1
arthurberman <arthurberman@hotmail.fr>
2021-12-02 09:02:33 +0100

    New image class

---
+++ ImageProcessing/src/main/java/Image.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aberman
+ */
+public class Image {
+    private int[][] imageMap;
+    
+    public Image(int[][] imageMap) {
+        this.imageMap = imageMap;
+    }
+}
